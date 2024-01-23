7ad57a130fbdf50130de5e51aa524edde7b3e47e
Mingguriguri <rlaalswjd-@naver.com>
2024-01-24 04:07:22 +0900

    [Like] DELETE like

--- todoApplication/todoApplication/src/main/java/com/example/winterproject/todoApplication/controller/LikeResource.java
+++ todoApplication/todoApplication/src/main/java/com/example/winterproject/todoApplication/controller/LikeResource.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.winterproject.todoApplication.domain.Follow;
+import com.example.winterproject.todoApplication.domain.FollowId;
 import com.example.winterproject.todoApplication.domain.Like;
 import com.example.winterproject.todoApplication.domain.LikeId;
 import com.example.winterproject.todoApplication.domain.Todo;
@@ -51,5 +54,24 @@ public class LikeResource {
 	    return ResponseEntity.ok("Liked.");
     }
 		
+    /*
+     * 좋아요 취소
+     */
+    // DELETE /cancel_like
+    @DeleteMapping("/cancel_like/{userId}/{todoId}")
+    public ResponseEntity<String> cancelLike(@PathVariable String userId, @PathVariable Integer todoId) {
+    	LikeId likeId = new LikeId();
+    	likeId.setUserId(userId);
+    	likeId.setTodoId(todoId);
+        
+        Optional<Like> likeOptional = likeRepository.findById(likeId);
 
+        if (likeOptional.isPresent()) {
+        	Like like = likeOptional.get();
+            likeRepository.delete(like);
+            return ResponseEntity.ok("Liked Canceled.");
+        } else {
+            return ResponseEntity.badRequest().body("Like not found.");
+        }
+    }
 }
